b9eb7c41c81b588b125986eda235e05b2fd56339
C. Spencer Beggs <spencer@savvyweb.systems>
2026-02-03 00:19:27 -0500

    chore: update dependencies
    Signed-off-by: C. Spencer Beggs <spencer@savvyweb.systems>

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,16 +1,11 @@
 import { NodeLibraryBuilder } from "@savvy-web/rslib-builder";
 
-const config: ReturnType<typeof NodeLibraryBuilder.create> = NodeLibraryBuilder.create({
-	tsdocLint: true,
-	// Externalize typescript - it uses __filename which doesn't work when bundled in ESM
-	// Also externalize source-map-support which is an optional typescript dependency
-	externals: [],
+export default NodeLibraryBuilder.create({
 	transform({ pkg }) {
 		delete pkg.devDependencies;
 		delete pkg.scripts;
 		delete pkg.publishConfig;
+		delete pkg.devEngines;
 		return pkg;
 	},
 });
-
-export default config;
